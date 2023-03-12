e010b19b14e041dfca1ffb53b12885024e312bb5
Harsha Lakamsani <hlakamsani@gmail.com>
2023-03-12 04:22:02 -0700

    node label formatting + colors

--- src/tree_config.js
+++ src/tree_config.js
@@ -3,7 +3,7 @@ import Tree from 'react-d3-tree';
 import './index.css'
 
 // example chart
-const db_chart = {
+const db_chart_1 = {
     name: 'img.location',
     attributes: 'projection',
     children: [
@@ -38,6 +38,82 @@ const db_chart = {
     ],
 };
 
+const db_chart_2 = {
+  name: 'img.date',
+  attributes: 'projection',
+  children: [
+    {
+      name: 'match(passengers.pic, img.face) = True',
+      attributes: 'natural_join',
+      children: [
+        {
+          name: 'img',
+          attributes: 'rename',
+          children: [
+            {
+              name: 'virtual_surveillance_images',
+              attributes: '',
+              children: []
+            },
+          ],
+        },
+        {
+          name: "name LIKE '\\%Peter Derr\\%' ⋀ datebirth'1982-06-05' ⋀ country='UK'",
+          attributes: 'selection',
+          children: [
+            {
+              name: 'passengers',
+              attributes: '',
+              children: []
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const db_chart_3 = {
+  name: 'img.location',
+  attributes: 'projection',
+  children: [
+    {
+      name: "img.date = '2023-03-07'",
+      attributes: 'natural_join',
+      children: [
+        {
+          name: 'match(passengers.pic, img.face) = True',
+          attributes: 'natural_join',
+          children: [
+            {
+              name: 'img',
+              attributes: 'rename',
+              children: [
+                {
+                  name: 'virtual_surveillance_images',
+                  attributes: '',
+                  children: []
+                },
+              ],
+            },
+            {
+              name: "name LIKE '\\%Peter Derr\\%' ⋀ datebirth'1982-06-05' ⋀ country='UK'",
+              attributes: 'selection',
+              children: [
+                {
+                  name: 'passengers',
+                  attributes: '',
+                  children: []
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
 function operation_symbol(op_id) {
   let symbol = ""
   if (op_id === 'projection') {
@@ -55,7 +131,7 @@ function operation_symbol(op_id) {
 
 function conditional_fill(nodeDatum) {
   if (nodeDatum.children.length !== 0) {
-    return "white"
+    return "beige"
   } else {
     return "#5CA904"
   };
@@ -72,9 +148,15 @@ const renderRectSvgNode = ({ nodeDatum, toggleNode}) => (
             <text class='label' x="0em" y="1em" alignment-baseline="middle" position='fixed' text-anchor="middle">{operation_symbol(nodeDatum.attributes)}</text>      
             {/* </foreignObject> */}
 
-            <text fill="blue" strokeWidth="1" x="20">
+            {/* <text class="force_wrap" fill="blue" strokeWidth="1" x="20">
                 {nodeDatum.name}
-            </text>
+            </text> */}
+
+    <foreignObject width="250" height="200" x="25" y="25">
+        <div class='wrap_break_box'>
+            <span>{nodeDatum.name}</span>
+        </div>
+    </foreignObject>
         </g>
     </React.Fragment>
 );
@@ -84,7 +166,7 @@ export const useCenteredTree = (defaultTranslate = { x: 0, y: 0 }) => {
   const containerRef = React.useCallback((containerElem) => {
     if (containerElem !== null) {
       const { width, height } = containerElem.getBoundingClientRect();
-      setTranslate({ x: width / 2, y: height / 2 });
+      setTranslate({ x: width / 2, y: height / 4 });
     }
   }, []);
   return [translate, containerRef];
@@ -96,15 +178,15 @@ export default function DatabaseTreeDisplay() {
   
   return (
     <div style={{width: '100%', height: '100vh'}} ref={containerRef}>
-      <Tree data={db_chart}
+      <Tree data={db_chart_3}
         translate={translate}
         orientation={"vertical"}
         allowForeignObjects
         renderCustomNodeElement={renderRectSvgNode}
 
         separation={{
-          siblings: 2,
-          nonSiblings: 2
+          siblings: 2.5,
+          nonSiblings: 2.5
         }}
       />
   </div>
