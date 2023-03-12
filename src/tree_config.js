929859072694584ca771609b2ef7e5845228b82c
Harsha Lakamsani <hlakamsani@gmail.com>
2023-03-11 22:42:46 -0700

    demo-style query rendering (fix operations)

--- src/tree_config.js
+++ src/tree_config.js
@@ -4,65 +4,75 @@ import './index.css'
 
 // example chart
 const db_chart = {
-    name: 'guestName',
-    attributes: {
-      ops: ['division', 'projection']
-    },
+    name: 'img.location',
+    attributes: 'projection',
     children: [
       {
-        name: 'hotelNo',
-        attributes: {
-          ops: ['projection']
-        },
+        name: 'match(passengers.pic, img.face) = True',
+        attributes: 'natural_join',
         children: [
           {
-            name: "city='London'",
-            attributes: {
-              ops: ['selection'],
-            },
-          },
-        ],
-      },
-      {
-        name: 'guestname, hotelNo',
-        attributes: {
-          ops: 'projection',
-        },
-        children: [
-          {
-            name: 'B.guestNo',
-            attributes: {
-              ops: 'natural-join',
-            },
+            name: 'img',
+            attributes: 'rename',
+            children: [
+              {
+                name: 'virtual_surveillance_images',
+                attributes: '',
+                children: []
+              },
+            ],
           },
           {
-            name: 'G.guestNo',
-            attributes: {
-              ops: 'natural-join'
-            },
-          }
+            name: "name LIKE '\\%Peter Derr\\%' ⋀ datebirth'1982-06-05' ⋀ country='UK'",
+            attributes: 'rename',
+            children: [
+              {
+                name: 'passengers',
+                attributes: '',
+                children: []
+              },
+            ],
+          },
         ],
       },
     ],
 };
 
+function operation_symbol(op_id) {
+  let symbol = ""
+  if (op_id === 'projection') {
+    symbol = 'Π'
+  } else if (op_id === 'natural_join') {
+    symbol = '⋈'
+  } else if (op_id === 'selection') {
+    symbol = 'σ'
+  } else if (op_id === 'rename') {
+    symbol = 'ρ'
+  };
+
+  return symbol
+}
+
 // refer to:
 // https://stackoverflow.com/questions/69416200/add-image-to-react-d3-tree-rendercustomnodeelement-attribute
 const renderRectSvgNode = ({ nodeDatum, toggleNode, 
-foreignObjectProps = {} }) => (
+foreignObjectProps = {x:0, y:0, width:20, height:20} }) => (
     <React.Fragment>
-        <foreignObject {...foreignObjectProps}>
-            <img src={'../public/logo192.png'} height="200" width="200"/>
-        </foreignObject>
         <g>
-            <circle cx="0" cy="20" r="20" fill="white" onClick={toggleNode} />
+            <circle cx="0" cy="20" r="20" fill="white" onClick={toggleNode}/>
+            <foreignObject {...foreignObjectProps}>
+                <text fill="blue" strokeWidth="1" x="20" y='20'>
+                    {operation_symbol(nodeDatum.attributes)}
+                </text>
+            </foreignObject>
+
             <text fill="blue" strokeWidth="1" x="20">
                 {nodeDatum.name}
             </text>
-                {nodeDatum.attributes?.department && (
+                {/* {nodeDatum.attributes && (
             <text fill="green" x="20" dy="20" strokeWidth="1">
-                Department: {nodeDatum.attributes}
-            </text>)}
+                Operation: {nodeDatum.attributes}
+            </text>)} */}
         </g>
     </React.Fragment>
 );
