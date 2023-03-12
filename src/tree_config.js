fba5cb68949ed38fc9335235125c59cb7e5e6012
Harsha Lakamsani <hlakamsani@gmail.com>
2023-03-11 21:29:52 -0700

    centered layout + custom node svg render

--- src/tree_config.js
+++ src/tree_config.js
@@ -46,24 +46,50 @@ const db_chart = {
     ],
 };
 
+// refer to:
+// https://stackoverflow.com/questions/69416200/add-image-to-react-d3-tree-rendercustomnodeelement-attribute
+const renderRectSvgNode = ({ nodeDatum, toggleNode, 
+foreignObjectProps = {} }) => (
+    <React.Fragment>
+        <foreignObject {...foreignObjectProps}>
+            <img src={'../public/logo192.png'} height="200" width="200"/>
+        </foreignObject>
+        <g>
+            <circle cx="0" cy="20" r="20" fill="white" onClick={toggleNode} />
+            <text fill="blue" strokeWidth="1" x="20">
+                {nodeDatum.name}
+            </text>
+                {nodeDatum.attributes?.department && (
+            <text fill="green" x="20" dy="20" strokeWidth="1">
+                Department: {nodeDatum.attributes}
+            </text>)}
+        </g>
+    </React.Fragment>
+);
+
+export const useCenteredTree = (defaultTranslate = { x: 0, y: 0 }) => {
+  const [translate, setTranslate] = React.useState(defaultTranslate);
+  const containerRef = React.useCallback((containerElem) => {
+    if (containerElem !== null) {
+      const { width, height } = containerElem.getBoundingClientRect();
+      setTranslate({ x: width / 2, y: height / 2 });
+    }
+  }, []);
+  return [translate, containerRef];
+};
+
 // component render function
 export default function DatabaseTreeDisplay() {
+  const [translate, containerRef] = useCenteredTree();
+  
   return (
-    <div style={{width: '100%', height: '100vh'}}>
+    <div style={{width: '100%', height: '100vh'}} ref={containerRef}>
       <Tree data={db_chart}
+        translate={translate}
         orientation={"vertical"}
         allowForeignObjects
-        // nodeLabelComponent={{
-        //   render: <CardSO />
-        // }}
-        // nodeSvgShape={svgSquare}
-        // pathFunc="step"
-        // translate={this.state.translate}
-        // initialDepth={1}
-        // nodeSize={{
-        //   x: 240,
-        //   y: 200
-        // }}
+        renderCustomNodeElement={renderRectSvgNode}
+
         separation={{
           siblings: 2,
           nonSiblings: 2
